582c1ab925bd8ad475acceac7629b6496c764a20
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-08-19 17:08:51 -0400

    Added drag/drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -357,14 +357,83 @@ $("#task-form-modal .btn-primary").click(function () {
   }
 });
 
-// remove all tasks
-$("#remove-tasks").on("click", function () {
-  for (var key in tasks) {
-    tasks[key].length = 0;
-    $("#list-" + key).empty();
-  }
-  saveTasks();
-});
+// make cards sortable
+$(".card .list-group").sortable({
+      connectWith: $(".card .list-group"),
+      scroll: false,
+      tolerance: "pointer",
+      helper: "clone",
+      activate: function (event) {
+        // console.log("activate", this);
+      },
+      deactivate: function (event) {
+        // console.log("deactivate", this);
+      },
+      over: function (event) {
+        // console.log("over", event.target);
+      },
+      out: function (event) {
+        // console.log("out", event.target);
+      },
+      update: function (event) {
+          // array to store the task data in
+          var tempArr = [];
 
-// load tasks for the first time
-loadTasks();
+          // loop over current set of children in sortable list
+          $(this).children().each(function () {
+            var text = $(this)
+              .find("p")
+              .text()
+              .trim();
+
+            var date = $(this)
+              .find("span")
+              .text()
+              .trim();
+
+            // add task data to the temp array as an object
+            tempArr.push({
+              text: text,
+              date: date
+            });
+
+            //trim down list's ID to match object and save
+            let arrName = $(this)
+              .attr('id')
+              .replace("list-", "");
+
+            //update array on tasks object and save
+            tasks[arrName] = tempArr;
+            saveTasks();
+
+            console.log(tempArr);
+          })
+        }
+      })
+
+      $("#trash").droppable({
+        accept: ".card .list-group-item",
+        tolerance: "touch",
+        drop: function(event, ui) {
+          ui.draggable.remove();
+          console.log("drop");
+        },
+        over: function(event, ui) {
+          console.log("over");
+        },
+        out: function(event, ui) {
+          console.log("out");
+        }
+      });
+      
+          // remove all tasks
+          $("#remove-tasks").on("click", function () {
+            for (var key in tasks) {
+              tasks[key].length = 0;
+              $("#list-" + key).empty();
+            }
+            saveTasks();
+          });
+
+          // load tasks for the first time
+          loadTasks();
